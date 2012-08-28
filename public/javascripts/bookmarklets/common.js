e0cf1c3b2b13ffa74131e973e6efc93e8837b07a
Daniel <dclochri@ubuntuvm.(none)>
2012-08-28 15:09:06 -0500

    Updated the Google DFP Bookmarklet

--- public/javascripts/bookmarklets/common.js
+++ public/javascripts/bookmarklets/common.js
@@ -8,7 +8,36 @@
  */
 
 var aps = aps || {}; // Juice Global Object
-var slots = [];
+aps.calledAds = [];
+aps.definedAds = [];
+aps.uncalledAds = [];
+aps.undefinedAds = [];
+aps.calledAdsLinks = [];
+aps.undefinedAdsLinks = [];
+
+/**
+ * Someone please reference the following CSS as a JS includes!!!!
+ * Time did not permit the opportunity.
+ */
+aps.css = 
+'<style type="text/css">'
++'#bkmrklt-report-wrapper {'
++'background-color:#FFFFFF;'
++'color:#000000;'
++'display:block;'
++'margin:0px;'
++'padding:0px;'
++'width:100%;'
++'}'
++'#bkmrklt-report-wrapper td,'
++'#bkmrklt-report-wrapper th {'
++'background-color:#EEEEEE;'
++'border:1px dotted #333333;'
++'padding:3px;'
++'vertical-align:top;'
++'}'
++'</style>';
+
 
 aps.getSlots = function() {
 	var scripts = document.getElementsByTagName('script');
@@ -17,7 +46,7 @@ aps.getSlots = function() {
 	for (var i=0; i < scripts.length; i++) {
 		var match = re.exec(scripts[i].innerHTML);
 		if (match) {
-			slots.push(match[1]);
+			aps.calledAds.push(match[1]);
 			this.highlightAd(match[1]);
 		}
 	}
@@ -30,7 +59,6 @@ aps.validateSlot = function(slot) {
 }
 
 aps.highlightAd = function(slot) {
-	console.log("Highlighting: " + slot);
 	if (this.validateSlot(slot)) {
 		var params = aps.getAdParams(slot);
 		var child = document.getElementById(slot);
@@ -39,7 +67,7 @@ aps.highlightAd = function(slot) {
 		var adEl = document.getElementById(slot);
 		parent.insertBefore(wrapper, child);
 		wrapper.id = slot + "-wrapper";
-		wrapper.class = "bookmarklet-wrapper";
+		wrapper.className += "bookmarklet-wrapper";
 		var content = "<strong>Ad Definition:</strong> " + slot + "<br />"; 
 		if (params) {
 			content += "<strong>Ad Unit:</strong> " + params.adUnit + "<br />"
@@ -57,8 +85,17 @@ aps.getAdParams = function(slot) {
 	var current = googletag.slot_manager_instance.H[slot] || null;
 	if (current) {
 		var kvps = [];
-		for (var i = 0; i < current.g.length; i++) 
-			 kvps.push(current.g[i][0] + "=" + current.g[i][1]);
+		
+		for (var pair in current.g) {
+   		var obj = current.g[pair];
+			for (var key in obj) {
+			  if (typeof obj[key] === 'string')
+        	kvps.push('<li>' + pair + " = " + obj[key] + '</li>');
+   		}
+		}
+			
+		kvps.sort();
+		kvps = (kvps.length > 0) ? '<ul>' + kvps.join(' ') + '</ul>' : 'N/A';
 		return {
 			adUnit: current.s || 'N/A'
 		,	kvps:	kvps || 'N/A'
@@ -67,11 +104,118 @@ aps.getAdParams = function(slot) {
 	return false;
 }
 
-aps.getCalledAds = function() {
-	try {
-		console.log(googletag.slot_manager_instance);
-	} catch(e) { }
+aps.addRow = function(key, value) {
+	return '<tr><th>' + key + '</th><td>' + value + '</td></tr>';
 }
 
+aps.getDebugReport = function() {
+	var wrapper = document.createElement("div");
+	wrapper.id = 'bkmrklt-report-wrapper';
+	var body = aps.css 
+	  + '<h1>Debug Bookmarklet Report</h1>'
+		+ '<table id="bkmrklt-table">'
+		+ aps.getURLData()
+	 	+ aps.getCalledAds()
+		+ aps.getDefinedAds()
+		+ aps.getUncalledAds()
+		+ aps.getUndefinedAds()
+		+ aps.getPageLevelKvps()
+  	+ '</table><br /><hr />'
+	;
+	wrapper.innerHTML = body;
+  if (document.body.firstChild)
+  	document.body.insertBefore(wrapper, document.body.firstChild);
+  else
+    document.body.appendChild(wrapper);
+}
+
+aps.getMatchedURL = function() {
+  var link = 'http://juice.hearstnp.com/DfpPage/Edit/' + APSpageID;
+  return this.addRow('Matched URL', 
+    '<p>Coming Soon -- this will tell you <em>exactly</em> what URL matched in Juice. For now, please see:<br />'
+  + '<a href="' + link + '">' + link + '</a></p>'
+  + '<p>This is the page (in Juice) that was used to target DFP.</p>'
+  );
+}
+
+aps.getURLData = function () {
+  var current = this.addRow('Current URL', document.URL);
+  var reported = this.addRow('Reported URL', 'http://' + JuicePageUrl);
+  var matched = this.getMatchedURL();
+  return current + reported + matched;
+}
+
+aps.getCalledAds = function() {
+  return this.addRow('Called Ads', aps.calledAdsLinks.sort().join(', '));
+}
+
+aps.getCalledAdsLinks = function() {
+  for (var i=0; i<aps.calledAds.length; i++)
+    aps.calledAdsLinks.push('<a href="#'+aps.calledAds[i]+'-wrapper">'+aps.calledAds[i]+'</a>');
+}
+
+aps.getDefinedAds = function() {
+  var slotMgr = googletag.slot_manager_instance.H;
+  for (var slot in slotMgr) {
+	  aps.definedAds.push(slot);
+	}
+	return this.addRow('Defined Ads', aps.definedAds.sort().join(', '));
+}
+
+aps.getUncalledAds = function () {
+  var def = aps.definedAds;
+  for (i=0; i<def.length; i++) {
+    if (!aps.calledAds.hasValue(def[i]))
+      aps.uncalledAds.push(def[i]);
+  }
+  return this.addRow('Uncalled Ads', aps.uncalledAds.sort().join(', ')); 
+}
+
+aps.getUndefinedAds = function () {
+  var called = aps.calledAds;
+  for (i=0; i<called.length; i++) {
+    if (!aps.definedAds.hasValue(called[i])) {
+      aps.undefinedAds.push(called[i]);
+    } 
+  }
+  this.getUndefinedAdsLinks();
+  return this.addRow('Undefined Ads', aps.undefinedAdsLinks.sort().join(', '));
+}
+
+aps.getUndefinedAdsLinks = function() {
+  for (var i=0; i<aps.undefinedAds.length; i++)
+    aps.undefinedAdsLinks.push('<a href="#'+aps.undefinedAds[i]+'-wrapper">'+aps.undefinedAds[i]+'</a>');
+}
+
+aps.getPageLevelKvps = function () {
+  var pageObj = googletag.pubads().g;
+  var kvps = [];
+  for (var pair in pageObj) {
+ 		var obj = pageObj[pair];
+		for (var key in obj) {
+		  if (typeof obj[key] === 'string')
+    	  kvps.push('<li>' + pair + " = " + obj[key] + '</li>');
+ 		}
+	}
+	return this.addRow('Page Level KVPs', '<ul>' + kvps.sort().join(', ') + '</ul>');
+}
+
+/**
+ * @static method:
+ * Extend the JS Array object so that we can test for  
+ * the existence of any value in an any array.
+ */
+Array.prototype.hasValue = function(value) {
+  var i;
+  for (i=0; i<this.length; i++) { if (this[i] === value) return true; }
+  return false;
+}
+
+/**
+ * Initialize and Exec 
+ */
 aps.getSlots();
 aps.getCalledAds();
+aps.getCalledAdsLinks();
+aps.getDebugReport();
+
